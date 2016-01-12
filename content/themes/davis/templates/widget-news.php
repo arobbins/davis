cccea098fde21c2b8044cf22e34fbeb8fe5e091d
arobbins <arobbins@simpleblend.net>
2016-01-11 21:24:38 -0600

    Added related news integration

--- content/themes/davis/templates/widget-news.php
+++ content/themes/davis/templates/widget-news.php
@@ -1,8 +1,15 @@
 <section class="l-box widget widget-related-news">
   <h1 class="widget-title"><?php echo $title; ?></h1>
   <?php
-    $args = array('numberposts' => 1);
+    global $post;
+    $currentPostID = $post->ID;
+
+    $cat = get_the_category($currentPostID);
+
+    $args = array('numberposts' => 3, 'category' => $cat[0]->term_id);
+
     $recent_posts = wp_get_recent_posts($args);
+
     foreach( $recent_posts as $recent ){
       echo '<a href="' . get_permalink($recent["ID"]) . '" class="widget-title-link"><h2>' .   $recent["post_title"].'</h2></a>';
       echo '<div class="widget-snippet">' . get_the_excerpt() . '</div>';
